02520313fbf8bd43ff02b7689d8ddba9688eb7f7
rain0416 <34537657+rain0416@users.noreply.github.com>
2020-06-28 23:53:05 -0700

    catch fruit game

---
+++ 接水果遊戲/接水果遊戲/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace 接水果遊戲
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(401, 31);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(72, 27);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(154, 30);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(14, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Remaining: 120 Seconds";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(14, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Received: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 323);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 接水果遊戲/接水果遊戲/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace 接水果遊戲
+{
+    public partial class Form1 : Form
+    {
+        Image[] background = { Properties.Resources._0, Properties.Resources._1, Properties.Resources._2, Properties.Resources._3,
+                               Properties.Resources._4, Properties.Resources._5, Properties.Resources._6, Properties.Resources._7 },
+                fruit = { Properties.Resources.Banana, Properties.Resources.StawBerry, Properties.Resources.Tomato },
+                nowfruit = new Image[3];
+
+        Image nowbackground, pedal = Properties.Resources.Bowl;
+
+        float[][] colormatrixarray = { new float[] {1, 0, 0, 0, 0 },
+                                       new float[] {0, 1, 0, 0, 0 },
+                                       new float[] {0, 0, 1, 0, 0 },
+                                       new float[] {0, 0, 0, 0.5f, 0 },
+                                       new float[] {0, 0, 0, 0, 1 } };
+
+        ImageAttributes imgattri = new ImageAttributes();
+
+        Random rand = new Random();
+
+        int fruitnum, second = 120, eat = 0;
+
+        Rectangle[] destrec = new Rectangle[3];
+
+        Rectangle pedalblock;
+
+        bool[] whetherdraw = { true, true, true };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+
+            nowbackground = background[rand.Next() % 8];
+
+            if(nowbackground.Width > this.ClientSize.Width || nowbackground.Height > this.ClientSize.Height - 168)
+
+                this.ClientSize = new Size(500, 300);
+
+            ColorMatrix colormatrix = new ColorMatrix(colormatrixarray);
+
+            imgattri.SetColorMatrix(colormatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+            pedalblock = new Rectangle(nowbackground.Width / 2 - pedal.Width / 2, nowbackground.Height + 31, pedal.Width, pedal.Height);
+
+            label1.Location = new Point(12, nowbackground.Height + pedal.Height + 34);
+
+            label2.Location = new Point(12, nowbackground.Height + pedal.Height + 59);
+
+            NewFruit();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int posx = e.Location.X;
+
+            if (posx > nowbackground.Width - pedal.Width / 2)
+
+                posx = nowbackground.Width - pedal.Width / 2;
+
+            else if (posx < pedal.Width / 2)
+
+                posx = pedal.Width / 2;
+
+            pedalblock = new Rectangle(posx - pedal.Width / 2, nowbackground.Height + 31, pedal.Width, pedal.Height);
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Remaining: 120 Seconds";
+
+            label2.Text = "Received: 0";
+
+            second = 120;
+
+            eat = 0;
+
+            timer2.Enabled = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(nowbackground, new Rectangle(0, 28, nowbackground.Width, nowbackground.Height), 0, 0, nowbackground.Width, nowbackground.Height, GraphicsUnit.Pixel, imgattri);
+
+            e.Graphics.DrawImage(pedal, pedalblock, new Rectangle(0, 0, pedal.Width, pedal.Height), GraphicsUnit.Pixel);
+
+            for (int i = 0; i < fruitnum; i++)
+            {
+                if (pedalblock.Y < destrec[i].Bottom && pedalblock.Y > destrec[i].Bottom - destrec[i].Height / 2 && second > 0)
+                    if (destrec[i].X + destrec[i].Width / 2 > pedalblock.X && destrec[i].X + destrec[i].Width / 2 < pedalblock.Right)
+                    {
+                        if(whetherdraw[i])
+
+                            label2.Text = "Received: " + ++eat;
+
+                        whetherdraw[i] = false;
+                    }
+
+                if (whetherdraw[i])
+                {
+                    e.Graphics.DrawImage(nowfruit[i], destrec[i], new Rectangle(0, 0, nowfruit[i].Width, nowfruit[i].Height), GraphicsUnit.Pixel);
+
+                    destrec[i].Y += 3;
+
+                    if (destrec[i].Y > this.ClientSize.Height)
+
+                        whetherdraw[i] = false;
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int backgroundindex;
+
+            second--;
+
+            if(second <= 1)
+
+                label1.Text = "Remaining: " + second + " Second";
+
+            else
+
+                label1.Text = "Remaining: " + second + " Seconds";
+
+            if (second == 0)
+            {
+                timer2.Enabled = false;
+
+                return;
+            }
+
+            if (second % 10 == 0)
+            {
+                backgroundindex = rand.Next() % 8;
+
+                while(background[backgroundindex] == nowbackground)
+
+                    backgroundindex = rand.Next() % 8;
+
+                nowbackground = background[backgroundindex];
+
+                if (nowbackground.Width > this.ClientSize.Width || nowbackground.Height > this.ClientSize.Height - 168)
+
+                    this.ClientSize = new Size(nowbackground.Width, nowbackground.Height + 168);
+            }
+        }
+
+        private void NewFruit()
+        {
+            bool[] selected = { false, false, false };
+
+            int fruitindex;
+
+            fruitnum = 1 + rand.Next() % 3;
+
+            for (int i = 0, posx, gap = 0; i < fruitnum; i++)
+            {
+                fruitindex = rand.Next() % 3;
+
+                while (selected[fruitindex])
+
+                    fruitindex = rand.Next() % 3;
+
+                selected[fruitindex] = true;
+
+                nowfruit[i] = fruit[fruitindex];
+
+                posx = rand.Next() % (nowbackground.Width - nowfruit[i].Width);
+
+                if (i > 0)
+
+                    gap = 80 + rand.Next() % 141;
+
+                destrec[i] = new Rectangle(posx, 28 - nowfruit[i].Height - gap, nowfruit[i].Width, nowfruit[i].Height);
+            }
+        }
+
+        private bool WhetherReset()
+        {
+            for (int i = 0; i < fruitnum; i++)
+                if (whetherdraw[i])
+                    return false;
+
+            return true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (WhetherReset() && second > 0)
+            {
+                for (int i = 0; i < fruitnum; i++)
+
+                    whetherdraw[i] = true;
+
+                NewFruit();
+            }
+
+            this.Invalidate();
+        }
+    }
+}
